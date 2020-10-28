8d18dfa69b3f49b171afa91a69142f59f89bcdf3
cornel-kim <cornelkemboi@yahoo.com>
2020-10-28 07:00:14 +0300

    first commit

--- app/src/main/java/com/modcom/linearlayout/MainActivity.kt
+++ app/src/main/java/com/modcom/linearlayout/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.modcom.linearlayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        explicit intent to open new actvity
+        OpenSecondActivity.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
